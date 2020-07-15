533e3e9ebd418d2d60810b877867ee1d596d93c1
eiab30p <eduardo.eddy.verde94@gmail.com>
2020-07-15 15:52:15 -0400

    enhancing snapshot process to stop, wait for instance to stop, snapshot, and then start

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -97,18 +97,30 @@ def instances():
     default=None,
     help="Only instances for project (tag Project:<name>)"
     )
-def create_snapshots(projects):
+def create_snapshots(project):
     "Create snapshots for EC2 instances"
 
     instances = filter_instances(project)
 
     for i in instances:
+        print("Stopping {0}...".format(i.id))
+
         i.stop()
+        i.wait_until_stopped()
+
         for v in i.volumes.all():
             print("Create snapshit of {0}".format(v.id))
             v.create_snapshot(Description="Created by SnapshotAnalyzer")
 
+        print("Starting {0}...".format(i.id))
+
+        i.start()
+        i.wait_until_running()
+
+    print("Job's done!")
+
     return
+
 
 @instances.command('list')
 @click.option(
